07aa021703d5479e1a0747b6cf662f982a58edde
AshwinAnand868 <anandashwin868@gmail.com>
2024-01-25 08:55:11 -0500

    Initialize project

---
+++ contactapi/src/main/java/io/services/contactapi/domain/Contact.java
@@ -0,0 +1,19 @@
+package io.services.contactapi.domain;
+
+/**
+ * @author AshwinAnand868
+ * @version 1.0
+ * @since 01/25/2024 (MM/dd/yyyy)
+ */
+public class Contact {
+    private String id;
+    private String name;
+    private String email;
+    private String title;
+    private String phone;
+    private String address;
+    private String status;
+    private String photoUrl;
+
+
+}
---
+++ contactapi/src/test/java/io/services/contactapi/ContactapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.services.contactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
